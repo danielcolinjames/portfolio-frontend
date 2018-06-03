11b5e9293fbc9cf9177a1bd5db41bddfd850a5b9
Daniel James <danielcolinjames@gmail.com>
2018-06-03 12:35:13 -0700

    add top bar to holding card

--- src/HoldingCard/HoldingCard.js
+++ src/HoldingCard/HoldingCard.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './HoldingCard.css';
 
-const HoldingCard = () => {
+// TODO: https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+import btc from '../images/logos/bitcoin-logo@3x.png';
+
+const HoldingCard = (props) => {
     return(
         <div className='holdingCard'>
-            
+            <div className='holdingCardTopBar'>
+                <div className='holdingCurrencyInfoContainer'>
+                    <img
+                        className='holdingCurrencyLogo'
+                        src={btc}
+                        alt='Bitcoin logo'/>
+                    <div className='holdingCurrencyInfo'>
+                        <div className='holdingCurrencySymbol'>{props.symbol}</div>
+                        <div className='holdingCurrencyName'>{props.name}</div>
+                    </div>
+                </div>
+                <div className='holdingCurrencyAmountContainer'>
+                    <div className='holdingCurrencyAmountInfo'>
+                        <p className='holdingCurrencyAmountLabel'>
+                            Amount
+                        </p>
+                        <p className='holdingCurrencyAmountBalance'>
+                            {props.balance}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
