9339cf43630253dae4a121f1b62ec904367b271e
Daniel James <danielcolinjames@gmail.com>
2018-06-03 12:35:44 -0700

    add value, profit, price fields to fake holding data

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import HoldingsList from './HoldingsList/HoldingsList';
 class App extends Component {
   state = {
     holdings: [
-      { symbol: 'BTC', name: 'Bitcoin', balance: 5.076634 },
-      { symbol: 'ETH', name: 'Ethereum', balance: 54466.34 },
-      { symbol: 'USD', name: 'US Dollar', balance: 4139.8 }
+      { symbol: 'BTC', name: 'Bitcoin', balance: 5.076634, value: 62114.27, profit: -334.16, price: 10566.08 },
+      { symbol: 'ETH', name: 'Ethereum', balance: 54466.34, value: 18377.64, profit: 445.22, price: 10566.08 },
+      { symbol: 'USD', name: 'US Dollar', balance: 4139.8, value: 0.5 }
     ]
   }
 
