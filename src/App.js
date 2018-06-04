f065290b7d2fa6e64cc36d016b3f3a9437e05999
Daniel James <danielcolinjames@gmail.com>
2018-06-03 18:50:28 -0700

    add valueChange prop

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import HoldingsList from './HoldingsList/HoldingsList';
 class App extends Component {
   state = {
     holdings: [
-      { symbol: 'BTC', name: 'Bitcoin', balance: 5.076634, value: 62114.27, profit: -334.16, price: 10566.08 },
-      { symbol: 'ETH', name: 'Ethereum', balance: 54466.34, value: 18377.64, profit: 445.22, price: 10566.08 },
-      { symbol: 'USD', name: 'US Dollar', balance: 4139.8, value: 0.5 }
+      { symbol: 'BTC', name: 'Bitcoin', balance: 5.076634, value: 62114.27, valueChange: -1.15, profit: -334.16, price: 10566.08 },
+      { symbol: 'ETH', name: 'Ethereum', balance: 54466.34, value: 18377.64, valueChange: 1.15, profit: 445.22, price: 10566.08 },
+      { symbol: 'USD', name: 'US Dollar', balance: 4139.8, value: 0.5, valueChange: 1.15 }
     ]
   }
 
@@ -29,6 +29,7 @@ class App extends Component {
                 name={holding.name}
                 balance={holding.balance}
                 value={holding.value}
+                valueChange={holding.valueChange}
                 profit={holding.profit}
                 price={holding.price}/>
             })}
