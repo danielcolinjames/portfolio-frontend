8aee7863c7ecd36290692d08ae537d6865fa015d
Keyvan Mosharraf <kayvanmsh@gmail.com>
2018-07-16 16:55:06 -0700

    Added components necessary for login flow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Switch} from 'react-router';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Home from './containers/Home';
+import Login from './containers/Login';
+import PrivateRoute from './containers/PrivateRoute';
 
-export default App;
+export default () => (
+  <Switch>
+    <Route exact path="/login/" component={Login} />
+    <PrivateRoute path="/" component={Home}/>
+  </Switch>
+);
---
+++ src/components/LoginForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export default class LoginForm extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+  handleInputChange = (event) => {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+    this.setState({
+      [name]: value
+    });
+  }
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.props.onSubmit(this.state.username, this.state.password)
+  }
+  render() {
+    const errors = this.props.errors || {}
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <h1>Authentication</h1>
+          {
+            errors.non_field_errors?
+              <p>
+                {errors.non_field_errors}
+              </p>:""
+          }
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={this.handleInputChange} />
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            onChange={this.handleInputChange}/>
+          <button type="submit">
+            Log In
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
