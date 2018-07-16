deb740adc483890676b1b0cb35ffddba6da1dc36
Keyvan Mosharraf <kayvanmsh@gmail.com>
2018-07-16 16:40:07 -0700

    - Redux persist - Refreshable API middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,11 +1,25 @@
-import { createStore, applyMiddleware } from 'redux'
-import { apiMiddleware } from 'redux-api-middleware';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import apiMiddleware from './middlewares/refreshableAPI';
+import storage from 'redux-persist/lib/storage';
+import createFilter from 'redux-persist-transform-filter';
 
-import rootReducer from './reducers'
+import rootReducer from './reducers';
 
+const authTokensFilter = createFilter('auth', ['access', 'refresh']);
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+  transforms: [authTokensFilter]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const createStoreWithMiddleware = applyMiddleware(apiMiddleware)(createStore);
 
 export default (initialState) => {
-  let store = createStoreWithMiddleware(rootReducer, initialState);
-  return store;
+  let store = createStoreWithMiddleware(persistedReducer, initialState);
+  let persistor = persistStore(store)
+  return { store, persistor }
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import { PersistGate } from 'redux-persist/integration/react'
 
 import configureStore from './configureStore';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
