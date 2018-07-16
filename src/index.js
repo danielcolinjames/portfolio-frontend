28932de8cfe899ce992f09526a82d5caa64e1bf8
Keyvan Mosharraf <kayvanmsh@gmail.com>
2018-07-16 15:42:58 -0700

    react-router set up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 
 import configureStore from './configureStore';
 import './index.css';
@@ -11,7 +13,9 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
