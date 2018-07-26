eeeb15a0e100e06c69e4550d211999cc5b470998
Daniel James <danielcolinjames@gmail.com>
2018-07-26 16:09:39 -0700

    Add styles to login page (incomplete) - the static page that Evan made is for sign up, but some of the styles are shared between the two pages so I used them for now - this will need changing in the future; the CSS needs to be separated into its own files (Login.css and Signup.css), and SignupForm needs to be developed (can be copied from LoginForm)

--- src/components/Authentication/LoginForm.js
+++ src/components/Authentication/LoginForm.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import './LoginForm.css';
 
 export default class LoginForm extends Component {
   state = {
@@ -20,29 +22,91 @@ export default class LoginForm extends Component {
   render() {
     const errors = this.props.errors || {}
     return (
+        // <div>
+        //   <form onSubmit={this.onSubmit}>
+        //     <h1>Log in</h1>
+        //     {
+        //       errors.non_field_errors?
+        //         <p>
+        //           {errors.non_field_errors}
+        //         </p>:""
+        //     }
+        //     <input
+        //       type="text"
+        //       name="username"
+        //       placeholder="Username"
+        //       onChange={this.handleInputChange} />
+        //     <input
+        //       name="password"
+        //       type="password"
+        //       placeholder="password"
+        //       onChange={this.handleInputChange}/>
+        //     <button type="submit">
+        //       Log In
+        //     </button>
+        //   </form>
+        // </div>
+
       <div>
-        <form onSubmit={this.onSubmit}>
-          <h1>Log in</h1>
-          {
-            errors.non_field_errors?
-              <p>
-                {errors.non_field_errors}
-              </p>:""
-          }
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={this.handleInputChange} />
-          <input
-            name="password"
-            type="password"
-            placeholder="password"
-            onChange={this.handleInputChange}/>
-          <button type="submit">
-            Log In
-          </button>
-        </form>
+        <div id="menu_bar">
+          <div id="logo"></div>
+          <div id="user_portal">Sign up</div>
+        </div>
+        <div id="signup_banner">
+          <div className="title_content">
+            <h1 className="white">Sign in</h1>
+          </div>
+        </div>
+
+        <div className="sheet_content">
+          <div className="row_flex">
+            <form
+              className="main_sheet"
+              onSubmit={this.onSubmit} >
+              {
+                errors.non_field_errors ?
+                  <p>
+                    {errors.non_field_errors}
+                  </p> : ""
+              }
+
+              <div className="textfield_area">
+                <h4>Email</h4>
+                <input
+                  type="text"
+                  name="username"
+                  autoFocus
+                  placeholder="name@example.com"
+                  onChange={this.handleInputChange}
+                  className="textfield" />
+              </div>
+              <div className="textfield_area">
+                <h4>Password</h4>
+                <input
+                  type="password"
+                  name="password"
+                  className="textfield"
+                  onChange={this.handleInputChange} />
+              </div>
+
+              {/* <div className="policy_check_area"><input className="sheet_checkbox" type="checkbox" name="policy_check" value="1" /> I agree to the <span className="text_link">User Agreement</span> and <span className="text_link">Privacy Policy</span></div> */}
+
+              {/* <div className="sheet_button  disabled">Sign in</div> */}
+
+              <button
+                type="submit"
+                className="sheet_button" >
+                Sign in
+              </button>
+            </form>
+
+            {/* <div className="side_sheet">
+              <h4 className="section_title">Or connect with</h4>
+              <div className="sheet_button"><div className="logo_coinbase"></div></div>
+            </div> */}
+          </div>
+        </div>
+
       </div>
     )
   }
